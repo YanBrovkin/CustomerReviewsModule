fd8f592d10002611ee25d03f78f2e80e9cee5293
Yan Brovkin <brovkin.yan@gmail.com>
2019-12-30 16:55:47 +0200

    feat #CCOP-106: added tests

--- CustomerReviewsModule.Tests/Services/CustomerReviewSearchServiceTests.cs
+++ CustomerReviewsModule.Tests/Services/CustomerReviewSearchServiceTests.cs
@@ -15,10 +15,10 @@ namespace CustomerReviewsModule.Tests.Services
 {
     public class CustomerReviewSearchServiceTests
     {
-        private Fixture randomizer;
-        private Mock<ICustomerReviewRepository> repository;
-        private Mock<ICustomerReviewService> customerReviewService;
-        private CustomerReviewSearchService service;
+        private readonly Fixture randomizer;
+        private readonly Mock<ICustomerReviewRepository> repository;
+        private readonly Mock<ICustomerReviewService> customerReviewService;
+        private readonly CustomerReviewSearchService service;
 
         public CustomerReviewSearchServiceTests()
         {
@@ -29,7 +29,7 @@ namespace CustomerReviewsModule.Tests.Services
         }
 
         [Fact]
-        public void SearchCustomerReviews_ShouldThrowArgumentException_IfCriteriaIsNull()
+        public void SearchCustomerReviews_ShouldThrowArgumentNullException_IfCriteriaIsNull()
         {
             //arrange
             CustomerReviewSearchCriteria criteria = null;
@@ -91,22 +91,25 @@ namespace CustomerReviewsModule.Tests.Services
         public void SearchCustomerReviews_ShouldReturnActiveProducts_IfIsActiveIsSet()
         {
             //arrange
-            //var productIds = randomizer.CreateMany<string>(2).ToArray();
+            var isActive = randomizer.Create<bool>();
             var criteria = new CustomerReviewSearchCriteria
             {
-
-                Take = 2
+                Take = 2,
+                IsActive = isActive
             };
 
-            var customerReviewEntities = randomizer.CreateMany<CustomerReviewEntity>(2);
-            //customerReviewEntities.ElementAt(0).ProductId = productIds[0];
+            var customerReviewEntities = randomizer
+                .Build<CustomerReviewEntity>()
+                .Without(p => p.IsActive)
+                .CreateMany(2);
+            customerReviewEntities.ElementAt(0).IsActive = isActive;
             repository.Setup(p => p.CustomerReviews).Returns(customerReviewEntities.AsQueryable());
 
             var customerReviews = randomizer.CreateMany<CustomerReview>(1);
             customerReviews.ElementAt(0).Id = customerReviewEntities.ElementAt(0).Id;
-            //customerReviewService
-            //    .Setup(p => p.GetByIds(It.IsAny<string[]>()))
-            //    .Returns(p => customerReviews.Where(r => p.Contains(r.Id)).ToArray());
+            customerReviewService
+                .Setup(p => p.GetByIds(It.IsAny<string[]>()))
+                .Returns(customerReviews.ToArray());
 
             //act
             var result = service.SearchCustomerReviews(criteria);
@@ -126,11 +129,58 @@ namespace CustomerReviewsModule.Tests.Services
                         ModifiedBy = customerReviews.ElementAt(0).ModifiedBy,
                         AuthorNickname = customerReviews.ElementAt(0).AuthorNickname,
                         Content = customerReviews.ElementAt(0).Content,
-                        IsActive = customerReviews.ElementAt(0).IsActive,
-                        //ProductId = productIds[0]
+                        IsActive = isActive,
+                        ProductId = customerReviews.ElementAt(0).ProductId
                     }
                 }
             });
+        }
+
+        [Fact]
+        public void SearchCustomerReviews_ShouldReturnProductsWithPhraseInContent_IfSearchPhraseIsSet()
+        {
+            //arrange
+            var searchPhrase = randomizer.Create<string>();
+            var criteria = new CustomerReviewSearchCriteria
+            {
+                Take = 2,
+                SearchPhrase = searchPhrase
+            };
+
+            var customerReviewEntities = randomizer.CreateMany<CustomerReviewEntity>(2);
+            customerReviewEntities.ElementAt(0).Content = searchPhrase;
+            repository.Setup(p => p.CustomerReviews).Returns(customerReviewEntities.AsQueryable());
+
+            var customerReviews = randomizer.CreateMany<CustomerReview>(1);
+            customerReviews.ElementAt(0).Id = customerReviewEntities.ElementAt(0).Id;
+            customerReviewService
+                .Setup(p => p.GetByIds(It.IsAny<string[]>()))
+                .Returns(customerReviews.ToArray());
+
+            //act
+            var result = service.SearchCustomerReviews(criteria);
+
+            //assert
+            result.Should().BeEquivalentTo(new GenericSearchResult<CustomerReview>
+            {
+                TotalCount = 1,
+                Results = new[]
+                {
+                    new CustomerReview
+                    {
+                        Id = customerReviews.ElementAt(0).Id,
+                        CreatedDate = customerReviews.ElementAt(0).CreatedDate,
+                        ModifiedDate = customerReviews.ElementAt(0).ModifiedDate,
+                        CreatedBy = customerReviews.ElementAt(0).CreatedBy,
+                        ModifiedBy = customerReviews.ElementAt(0).ModifiedBy,
+                        AuthorNickname = customerReviews.ElementAt(0).AuthorNickname,
+                        Content = searchPhrase,
+                        IsActive = customerReviews.ElementAt(0).IsActive,
+                        ProductId = customerReviews.ElementAt(0).ProductId
+                    }
+                }
+            });
+
         }
     }
 }
